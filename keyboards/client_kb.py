5a9b306567167e2c63f8721e33e18f283b803825
LynnuiYogurt <wanosgaming@gmail.com>
2023-02-07 20:02:50 +0200

    update structure

--- keyboards/client_kb.py
+++ keyboards/client_kb.py
@@ -1,10 +1,9 @@
 
 
-from aiogram.utils.callback_data import CallbackData
+
 from aiogram.types import ReplyKeyboardMarkup, KeyboardButton,InlineKeyboardMarkup,InlineKeyboardButton,ReplyKeyboardRemove
 
 
-cb_inline = CallbackData("post", 'action', "data")
 
 chek_name = KeyboardButton('Перевірка за назвою')
 lists = KeyboardButton('Списки компаній')
@@ -12,10 +11,10 @@ chek_photo = KeyboardButton('Перевірка за фото')
 
 kb_client_global = ReplyKeyboardMarkup(resize_keyboard=True)
 
-ua = InlineKeyboardButton(text = '🇺🇦Українські компанії',callback_data=cb_inline.new(action='ua', data='MyText'))
-left = InlineKeyboardButton(text = '🟢Компанії, що покинули ринок',callback_data=cb_inline.new(action='left', data='MyText1'))
-stay = InlineKeyboardButton(text = '🔴Компанії, що залишилися на ринку',callback_data=cb_inline.new(action='stay', data='MyText2'))
-root = InlineKeyboardButton(text = '🔴🔴Компанії з ришистським корінням',callback_data=cb_inline.new(action='root', data='MyText3'))
+ua = InlineKeyboardButton(text = '🇺🇦Українські компанії',callback_data='ua')
+left = InlineKeyboardButton(text = '🟢Компанії, що покинули ринок',callback_data='left')
+stay = InlineKeyboardButton(text = '🔴Компанії, що залишилися на ринку',callback_data='stay')
+root = InlineKeyboardButton(text = '🔴🔴Компанії з ришистським корінням',callback_data='root')
 
 
 
