5cce079ffe4c688a1577d18def71f51e80861ab1
LynnuiYogurt <wanosgaming@gmail.com>
2023-02-19 13:41:35 +0200

    initial

--- create_bot.py
+++ create_bot.py
@@ -2,8 +2,10 @@ from aiogram import Bot
 from aiogram.dispatcher import Dispatcher
 import os
 from dotenv import load_dotenv
+from aiogram.contrib.fsm_storage.memory import MemoryStorage
+
 
 load_dotenv()
-
+storage = MemoryStorage()
 bot = Bot(token=os.getenv('TOKEN'))
-dp = Dispatcher(bot)
+dp = Dispatcher(bot,storage=storage)
